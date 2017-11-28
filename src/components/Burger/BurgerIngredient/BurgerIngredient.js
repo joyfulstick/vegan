d130cd8bee0b87c7a6d246fe47148581fa80f961
joyfulstick <m.kijewicz@gmail.com>
2017-11-28 18:40:55 +0100

    Rendering a burgegr

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import classes from './BurgerIngredient.js';
+import classes from './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
-  rednder() {
+  render() {
     let ingredient = null;
     switch (this.props.type) {
     case ('bread-bottom'):
