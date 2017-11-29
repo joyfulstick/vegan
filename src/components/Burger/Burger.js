9ba1598459ed3e73ccc9898f8c10252e01328d14
joyfulstick <m.kijewicz@gmail.com>
2017-11-29 01:31:25 +0100

    Adding the Order button

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,22 +3,22 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  let transformedIgredients = Object.keys(props.igredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
-      return [...Array(props.igredients[igKey])].map((_, i) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
       });
     })
     .reduce((arr, el) => {
       return arr.concat(el);
     }, []);
-  if (transformedIgredients.length === 0) {
-    transformedIgredients = <p>Please start adding ingredients!</p>;
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
   }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      {transformedIgredients}
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
