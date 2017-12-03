942278908cb595aca5e7de20474c323c954d922d
joyfulstick <m.kijewicz@gmail.com>
2017-12-03 22:23:06 +0100

    Sending a POST request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.7,
@@ -76,7 +77,23 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Michał Kijewicz',
+        addres: {
+          street: 'Berezyńska 13',
+          zipCode: '03-904',
+          country: 'Poland'
+        },
+        email: 'kijewicz@gmail.com'
+      },
+      deliveryMethod: 'standard'
+    };
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
   render() {
