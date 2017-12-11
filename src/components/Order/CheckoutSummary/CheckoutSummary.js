84a7b3b668830bde7340bf75f5309bf185bab1f3
joyfulstick <m.kijewicz@gmail.com>
2017-12-11 23:01:14 +0100

    Fetching orders

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -9,12 +9,14 @@ const checkoutSummary = props => {
       <h1>We hope it tastes well!</h1>
       <div style={{ width: '100%', margin: 'auto' }}>
         <Burger ingredients={props.ingredients} />
-        <Button
-          btnType="Danger"
-          clicked={props.checkoutCancelled}>CANCEL</Button>
-        <Button
-          btnType="Success"
-          clicked={props.checkoutContinued}>CONTINUE</Button>
+        <div className={classes.CheckoutOptions}>
+          <Button
+            btnType="Danger"
+            clicked={props.checkoutCancelled}>CANCEL</Button>
+          <Button
+            btnType="Success"
+            clicked={props.checkoutContinued}>CONTINUE</Button>
+        </div>
       </div>
     </div>
   );
