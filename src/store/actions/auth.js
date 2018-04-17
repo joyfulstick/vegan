562d02ad72ab21796ad323893cff4a52befcd18d
joyfulstick <m.kijewicz@gmail.com>
2018-04-17 23:00:51 +0200

    Get a token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import * as actionTypes from './actionsTypes'
 
 export const authStart = () => {
@@ -23,5 +24,23 @@ export const authFail = error => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart())
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true,
+    }
+    axios
+      .post(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBv9KdBd_xzmJnrXD3kN8keWYwhvMB7dPo',
+        authData,
+      )
+      .then(response => {
+        console.log(response)
+        dispatch(authSuccess(response.data))
+      })
+      .catch(err => {
+        console.log(err)
+        dispatch(authFail(err))
+      })
   }
 }
