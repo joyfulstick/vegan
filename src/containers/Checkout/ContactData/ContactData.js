f6690fd845610a7a25857ce701adda60daafa0c6
joyfulstick <m.kijewicz@gmail.com>
2018-07-10 20:35:29 +0200

    Display user specific orders

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -130,6 +130,7 @@ class ContactData extends Component {
       ingredients: this.props.ings,
       price: this.props.price,
       orderData: formData,
+      userId: this.props.userId,
     }
     this.props.onOrderBurger(order, this.props.token)
   }
@@ -202,16 +203,19 @@ const mapStateToProps = state => {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
-    token: state.auth.token
+    token: state.auth.token,
+    userId: state.auth.userId,
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: (orderData, token) => dispatch(actions.puchaseBurger(orderData, token)),
+    onOrderBurger: (orderData, token) =>
+      dispatch(actions.puchaseBurger(orderData, token)),
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(
-  withErrorHandler(ContactData, axios),
-)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withErrorHandler(ContactData, axios))
