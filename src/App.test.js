5d32f6df42718b85ee6ce771fa9ecf42ab94e7e7
joyfulstick <m.kijewicz@gmail.com>
2018-07-27 22:04:25 +0200

    test the NavigationsItems component

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-it('renders without crashing', () => {
+xit('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
 })
