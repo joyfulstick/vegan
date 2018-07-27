a3b374a4d7c787adf865dbafc429187b7f5073d1
joyfulstick <m.kijewicz@gmail.com>
2018-07-27 19:53:42 +0200

    use environment variables

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ import orderReducer from './store/reducers/order'
 import registerServiceWorker from './registerServiceWorker'
 import thunk from 'redux-thunk'
 
+// const composeEnhancers =
+//   process.env.NODE_ENV === 'development'
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     : null || compose
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const rootReducer = combineReducers({
   auth: authReducer,
